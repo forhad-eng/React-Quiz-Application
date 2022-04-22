1ef78108aa4b2e252f4052c63792d12ea28c5b28
forhad-eng <forhadu.cse@gmail.com>
2022-04-22 21:07:24 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+const Quiz = [
+    {
+        question_text: 'What is the capital of Bangladesh?',
+        answer_options: [
+            { option_one: 'Chittagong', isCorrect: false },
+            { option_two: 'Rajshahi', isCorrect: false },
+            { option_three: 'Dhaka', isCorrect: true },
+            { option_four: 'Barisal', isCorrect: false }
+        ]
+    },
+    {
+        question_text: 'How many districts are there in Bangladesh?',
+        answer_options: [
+            { option_one: '48', isCorrect: false },
+            { option_two: '64', isCorrect: true },
+            { option_three: '65', isCorrect: false },
+            { option_four: '38', isCorrect: false }
+        ]
+    },
+    {
+        question_text: 'Who is the current President of Bangladesh?',
+        answer_options: [
+            { option_one: 'Obaidul Kader', isCorrect: false },
+            { option_two: 'Shiekh Hasina', isCorrect: false },
+            { option_three: 'Abdul Hamid', isCorrect: true },
+            { option_four: 'AHM Mostafa Kamal', isCorrect: false }
+        ]
+    },
+    {
+        question_text: 'Bangladesh is a country of?',
+        answer_options: [
+            { option_one: 'Europe', isCorrect: false },
+            { option_two: 'Africa', isCorrect: false },
+            { option_three: 'North America', isCorrect: false },
+            { option_four: 'Asia', isCorrect: true }
+        ]
+    }
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="bg-blue-400 h-screen flex justify-center items-center">
+            <div className="w-1/3 flex justify-between p-4 bg-[#231c40] rounded-2xl text-white">
+                <div>
+                    <p>Question 1/4</p>
+                </div>
+                <div>
+                    <p>Dhaka</p>
+                    <p>Harare</p>
+                    <p>Cape Town</p>
+                    <p>Accra</p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
