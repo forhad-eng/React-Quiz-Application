dc809ee9116adda8357b6430d9ff72020c88de3a
forhad-eng <forhadu.cse@gmail.com>
2022-04-22 21:19:05 +0600

    UI and some some functionality added

--- src/App.js
+++ src/App.js
@@ -1,54 +1,58 @@
-const Quiz = [
+import { useState } from 'react'
+
+const quizzes = [
     {
         question_text: 'What is the capital of Bangladesh?',
         answer_options: [
-            { option_one: 'Chittagong', isCorrect: false },
-            { option_two: 'Rajshahi', isCorrect: false },
-            { option_three: 'Dhaka', isCorrect: true },
-            { option_four: 'Barisal', isCorrect: false }
+            { option: 'Chittagong', isCorrect: false },
+            { option: 'Rajshahi', isCorrect: false },
+            { option: 'Dhaka', isCorrect: true },
+            { option: 'Barisal', isCorrect: false }
         ]
     },
     {
         question_text: 'How many districts are there in Bangladesh?',
         answer_options: [
-            { option_one: '48', isCorrect: false },
-            { option_two: '64', isCorrect: true },
-            { option_three: '65', isCorrect: false },
-            { option_four: '38', isCorrect: false }
+            { option: '48', isCorrect: false },
+            { option: '64', isCorrect: true },
+            { option: '65', isCorrect: false },
+            { option: '38', isCorrect: false }
         ]
     },
     {
         question_text: 'Who is the current President of Bangladesh?',
         answer_options: [
-            { option_one: 'Obaidul Kader', isCorrect: false },
-            { option_two: 'Shiekh Hasina', isCorrect: false },
-            { option_three: 'Abdul Hamid', isCorrect: true },
-            { option_four: 'AHM Mostafa Kamal', isCorrect: false }
+            { option: 'Obaidul Kader', isCorrect: false },
+            { option: 'Shiekh Hasina', isCorrect: false },
+            { option: 'Abdul Hamid', isCorrect: true },
+            { option: 'AHM Mostafa Kamal', isCorrect: false }
         ]
     },
     {
         question_text: 'Bangladesh is a country of?',
         answer_options: [
-            { option_one: 'Europe', isCorrect: false },
-            { option_two: 'Africa', isCorrect: false },
-            { option_three: 'North America', isCorrect: false },
-            { option_four: 'Asia', isCorrect: true }
+            { option: 'Europe', isCorrect: false },
+            { option: 'Africa', isCorrect: false },
+            { option: 'North America', isCorrect: false },
+            { option: 'Asia', isCorrect: true }
         ]
     }
 ]
 
 function App() {
+    const [currentQuestion, setCurrentQuestion] = useState(0)
+
     return (
         <div className="bg-blue-400 h-screen flex justify-center items-center">
             <div className="w-1/3 flex justify-between p-4 bg-[#231c40] rounded-2xl text-white">
                 <div>
                     <p>Question 1/4</p>
+                    {quizzes[currentQuestion].question_text}
                 </div>
                 <div>
-                    <p>Dhaka</p>
-                    <p>Harare</p>
-                    <p>Cape Town</p>
-                    <p>Accra</p>
+                    {quizzes[currentQuestion].answer_options.map(option => (
+                        <button className="w-full block border-2 rounded-lg my-3 p-1">{option.option}</button>
+                    ))}
                 </div>
             </div>
         </div>
